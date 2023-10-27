4f86f3b26f20df22a32336572a1d883a77c52b9c
ivo garofalo <ivo.garofalo@gmail.com>
2023-10-27 16:53:05 +0200

    init_commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,28 @@
-import { getDefaultConfig } from 'connectkit'
-import { createConfig } from 'wagmi'
+import { configureChains, mainnet, createConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { getDefaultConfig } from 'connectkit';
+import { polygon, polygonZkEvm, polygonZkEvmTestnet } from 'wagmi/chains';
 
-const walletConnectProjectId = '410e3ee66a03487c8e932b383ffa6de8'
+const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string;
+const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string;
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, polygon, polygonZkEvm, polygonZkEvmTestnet],
+  [alchemyProvider({ apiKey: alchemyApiKey  }), publicProvider()],
+)
 
 export const config = createConfig(
   getDefaultConfig({
+    publicClient,
+    webSocketPublicClient,
     autoConnect: true,
-    appName: 'My wagmi + ConnectKit App',
+    appName: 'Web3 Patient Portal',
     walletConnectProjectId,
   })
 )
+
+
+
+
+ 
