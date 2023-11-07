66d7d07c8ec1b30016cb5b9b1c26fcf919c7bbd8
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-11-08 00:26:32 +0100

    Refactor code to improve readability and maintainability.

--- frontend/src/app/issuer/page.tsx
+++ frontend/src/app/issuer/page.tsx
@@ -2,118 +2,181 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
+const dockUrl = process.env.NEXT_PUBLIC_TEST_URL as string;
+const apiToken = 'eyJzY29wZXMiOlsidGVzdCIsImFsbCJdLCJzdWIiOiIxMDg5MSIsInNlbGVjdGVkVGVhbUlkIjoiMTUwMTIiLCJjcmVhdG9ySWQiOiIxMDg5MSIsImlhdCI6MTY5OTMwNTE3MSwiZXhwIjo0Nzc4NjAxMTcxfQ.nUnHQyBE1qz59oKALpQtDehxRZal1-ozdA59YnVI3A2W9KrulEUs1Ltga3rKdKlRUjHrHd8XE61MlE2o9sdLCg';
+
 
 interface SchemaMetadata {
-  jsonLdContext: string;
-  jsonSchema: string;
+    jsonLdContext: string;
+    jsonSchema: string;
 }
 
 interface SchemaProperties {
-  [key: string]: any; // Replace 'any' with a more specific type if possible
+    [key: string]: any;
 }
 
 interface SchemaDefinition {
-  $metadata: SchemaMetadata;
-  $schema: string;
-  description: string;
-  properties: SchemaProperties;
-  required: string[];
-  type: string;
-  name: string;
-  additionalProperties: boolean;
-  $id: string;
+    $metadata: SchemaMetadata;
+    $schema: string;
+    description: string;
+    properties: SchemaProperties;
+    required: string[];
+    type: string;
+    name: string;
+    additionalProperties: boolean;
+    $id: string;
 }
 
 interface SchemaItem {
-  schema: SchemaDefinition;
-  id: string;
-  uri: string;
-  created: string;
+    schema: SchemaDefinition;
+    id: string;
+    uri: string;
+    created: string;
 }
 
 const SchemaCard: React.FC<{ schema: SchemaDefinition }> = ({ schema }) => {
-  // Implement the SchemaCard component
-  return (
-    <div className="border rounded-lg p-4">
-      <h3 className="text-lg font-medium">{schema.name}</h3>
-      <p>{schema.description}</p>
-      {/* Render more schema details here */}
-    </div>
-  );
+    // Implement the SchemaCard component
+    return (
+        <div className="border rounded-lg p-4">
+            <h3 className="text-lg font-medium">{schema.name}</h3>
+            <p>{schema.description}</p>
+            {/* Render more schema details here */}
+        </div>
+    );
 };
 
 const SchemasPage: React.FC = () => {
-  const [schemas, setSchemas] = useState<SchemaItem[]>([]);
-  const [selectedSchemaId, setSelectedSchemaId] = useState<string>('');
-  const [selectedSchema, setSelectedSchema] = useState<SchemaDefinition | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [schemas, setSchemas] = useState<SchemaItem[]>([]);
+    const [selectedSchemaId, setSelectedSchemaId] = useState<string>('');
+    const [selectedSchema, setSelectedSchema] = useState<SchemaDefinition | null>(null);
+    const [credentialQR, setCredentialQR] = useState<string>('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/list-schemas');
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-        const data = await response.json();
-        setSchemas(data); // Assuming the API returns the array directly
-      } catch (error) {
-        console.error('Failed to fetch schemas:', error);
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('/api/list-schemas');
+                if (!response.ok) {
+                    throw new Error(`Error ${response.status}: ${response.statusText}`);
+                }
+                const data = await response.json();
+                // Assuming the API returns an object with a 'schemas' key containing the array
+                if (data) {
+                    console.log("data:", data.data)
+                    setSchemas(data.data);
+                } else {
+                    // Handle the case where data.schemas is not an array
+                    console.error('Data is not an array:', data);
+                    setSchemas([]); // Set to an empty array or handle accordingly
+                }
+            } catch (error) {
+                console.error('Failed to fetch schemas:', error);
+                setSchemas([]); // Fallback to an empty array in case of error
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+
+
+        fetchData();
+    }, []);
+
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const schemaId = event.target.value;
+        setSelectedSchemaId(schemaId);
+        const schemaItem = schemas?.find((schema) => schema.id === schemaId);
+        setSelectedSchema(schemaItem ? schemaItem.schema : null);
     };
 
-    fetchData();
-  }, []);
+    const issueCredential = async () => {
+        setIsLoading(true);
+        if (!selectedSchemaId) {
+            console.error('No schema selected');
+            return;
+        }
 
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const schemaId = event.target.value;
-    setSelectedSchemaId(schemaId);
-    const schemaItem = schemas.find((schema) => schema.id === schemaId);
-    setSelectedSchema(schemaItem ? schemaItem.schema : null);
-  };
+        try {
+            const response = await fetch(`${dockUrl}/credentials`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'DOCK-API-TOKEN': apiToken,
+                },
+                body: JSON.stringify({
+                    "credential": {
+                        "id": "http://example.com/39",
+                        "context": ["https://www.w3.org/2018/credentials/examples/v1"],
+                        "type": [
+                            "VerifiableCredential"
+                        ],
+                        "subject": {
+                            "id": "did:dock:5ELhyt6HMBqBsurH2cYtnYQC2n4BEJkvXwCsiwgegHv3EXiD"
+                        },
+                        "issuer": {
+                            "id": "did:dock:5ELhyt6HMBqBsurH2cYtnYQC2n4BEJkvXwCsiwgegHv3EXiD",
+                            "name": "VBS - Issuer"
+                        }
+                    }
+                }),
+            });
 
-  return (
-    <div className="p-4">
-      <div className="flex flex-row gap-4 mb-8">
-        <div className="w-1/2">
-          <label htmlFor="schema-select" className="block text-sm font-medium text-gray-700">
-            Select Credential Schema
-          </label>
-          <select
-            id="schema-select"
-            value={selectedSchemaId}
-            onChange={handleSelectChange}
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-          >
-            <option value="">Select a schema...</option>
-            {schemas.map((schema) => (
-              <option key={schema.id} value={schema.id}>
-                {schema.schema.name}
-              </option>
-            ))}
-          </select>
+            if (!response.ok) {
+                throw new Error(`Failed to issue credential: ${response.statusText}`);
+            }
+
+            const credentialData = await response.json();
+            // Assuming credentialData contains a property 'qrCodeData' that we need to display
+            setCredentialQR(credentialData.qrCodeData);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false); // Hide loading feedback
+        }
+    };
+
+    return (
+        <div className="p-4">
+            {isLoading && <div>Loading...</div>}
+            <div className="flex flex-row gap-4 mb-8">
+                <div className="w-1/2">
+                    <label htmlFor="schema-select" className="block text-sm font-medium text-gray-700">
+                        Select Credential Schema
+                    </label>
+                    <select
+                        id="schema-select"
+                        value={selectedSchemaId}
+                        onChange={handleSelectChange}
+                        className="..."
+                    >
+                        <option value="">Select a schema...</option>
+                        {Array.isArray(schemas) && schemas.map((schemaItem) => (
+                            <option key={schemaItem.id} value={schemaItem.id}>
+                                {schemaItem.schema.name}
+                            </option>
+                        ))}
+                    </select>
+
+                </div>
+                <div className="w-1/2">
+                    {selectedSchema && <SchemaCard schema={selectedSchema} />}
+                </div>
+            </div>
+
+            {/* New row for issuing the credential */}
+            <div className="mt-8">
+                <button onClick={issueCredential} className="mt-4">
+                    Issue Credential
+                </button>
+                {credentialQR && (
+                    <div className="mt-4">
+                        <QRCodeSVG value={credentialQR} />
+                    </div>
+                )}
+            </div>
         </div>
-        <div className="w-1/2">
-          {selectedSchema && <SchemaCard schema={selectedSchema} />}
-        </div>
-      </div>
-
-       {/* New row for issuing the credential */}
-       <div className="mt-8">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={issueCredential}
-        >
-          Issue Credential
-        </button>
-
-        {qrCodeData && (
-          <div className="mt-4">
-            <QRCodeSVG value={qrCodeData} />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SchemasPage;
