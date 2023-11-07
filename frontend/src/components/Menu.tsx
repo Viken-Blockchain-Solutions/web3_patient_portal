300c7a6b5efeee5e06b140f36da55a9431557b00
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-11-07 20:28:45 +0100

    Update link text for patient page

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
     <div className='flex gap-3 flex-row mb-4'>
     <Link href="/" className='text-blue-500'>Wagmi</Link>
     <Link href="/home" className='text-blue-500'>Home</Link>
-    <Link href="/patient" className='text-blue-500'>Bruker</Link>
+    <Link href="/patient" className='text-blue-500'>User</Link>
     <Link href="/institute" className='text-blue-500'>Research Organisation</Link>
     </div>
   )
