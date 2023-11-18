5773b79cd88ce2c8b385a34be117562a661f86c7
ivo garofalo <ivo.garofalo@gmail.com>
2023-11-18 15:00:59 +0100

    Remove unused import statement

--- frontend/src/components/PoolCard.tsx
+++ frontend/src/components/PoolCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-import React, { useState } from "react";
 import Image from "next/image";
 import Docs from "../public/assets/images/docs.png";
 import PoolModal from "./pools/PoolModal";
