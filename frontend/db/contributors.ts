61b6e9be97c63f32c4548aea2d886ae77f5b4874
ivo garofalo <ivo.garofalo@gmail.com>
2023-11-24 12:20:57 +0100

    Add contributor existence check and handle adding new contributor

---
+++ frontend/db/contributors.ts
@@ -0,0 +1,44 @@
+// frontend/db/contributors.ts
+import { supabase } from "./supabaseClient";
+
+export const checkContributorExists = async (contributorDid: string): Promise<boolean> => {
+  const { data: existingContributors, error } = await supabase
+    .from("contributors")
+    .select("*")
+    .eq("contributor_did", contributorDid.split("did:key:")[1]);
+
+  if (error) {
+    console.log("user already exists:", error);
+    return false;
+  }
+  console.log(existingContributors);
+  return existingContributors.length > 0;
+};
+
+export const handleAddContributor = async (_userDid: string) => {
+  const keyPart = _userDid.split("did:key:")[1];
+  const exists = await checkContributorExists(keyPart);
+  if (!exists) {
+    const { data: newContributor, error } = await supabase
+      .from("contributors")
+      .insert([{ contributor_did: keyPart }]);
+
+    if (error) {
+      console.error("Error in adding contributor:", error);
+      return { success: false, error: error.message };
+    }
+
+    console.log(`New contributor added: ${newContributor}`,);
+
+    return {
+      success: true
+    };
+
+  } else {
+    console.log("Contributor already exists");
+    return {
+      success: false,
+      error: "Contributor already exists"
+    };
+  }
+};
--- frontend/stores/appStore.ts
+++ frontend/stores/appStore.ts
@@ -1,6 +1,16 @@
-import { create } from 'zustand'
+import { handleAddContributor } from "../db/contributors";
+import { create } from "zustand";
 
 export const userStore = create((set) => ({
-    Did: '',
-    setDid: (did: string) => set(() => ({ Did: did })),
-}))
+  Did: "",
+  setDid: async (did: string) => {
+    try {
+      console.log("addingDID:", did);
+      const result = await handleAddContributor(did);
+      set(() => ({ Did: did }));
+      return result;
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+  }
+}));
